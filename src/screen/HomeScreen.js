6c41f2ce50a37f84749cd9aba7fc82e9d4f27039
pratama11-dev <adithyanuzpratamaputra@gmail.com>
2022-05-17 18:27:31 +0700

    [WIP] button filter nation and infinit scroll

--- src/screen/HomeScreen.js
+++ src/screen/HomeScreen.js
@@ -1,13 +1,18 @@
 import axios from "axios";
 import { useEffect, useState } from 'react';
-import { Card, Avatar, Modal } from 'antd';
+import { Card, Avatar, Modal, Button } from 'antd';
 import { EyeOutlined } from '@ant-design/icons';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 import ModalScreen from '../screen/ModalScreen';
 
-export default function HomeScreen() {
+
+export default function HomeScreen(props) {
   const [ posts, setPosts ] = useState();
+  const [ nat, setNat ] = useState();
+
+  const PAGE_NUMBER = 1;
+  const [page, setPage] = useState(PAGE_NUMBER);
 
   const { Meta } = Card;
 
@@ -46,20 +51,38 @@ export default function HomeScreen() {
     setIsModalVisible(false);
   };
 
+  // useEffect(() => {
+  //   const fetchNat = async () => {
+  //     const {data} = await axios.get(`https://randomuser.me/api/?page=1&results=10&nat=gb`);
+  //     setNat(data);
+  //     console.log(data);
+  //   }
+  //   fetchNat();
+  // }, []);
+
   useEffect(()=> {
     const fetchData = async () => {
       try {
         setLoading(true);
-        const { data } = await axios.get("https://randomuser.me/api/?page=1&results=50");
+        const { data } = await axios.get(`https://randomuser.me/api/?page=${page}&results=20`);
         setLoading(false);
         setPosts(data);
-        // debugger
       } catch (error) {
         setError(error.message);
         setLoading(false);
       }}
       fetchData();
-  },[]);
+  },[page]);
+
+  const scrollToEnd = () => {
+    setPage(page + 1);
+  }
+
+  window.onscroll = function() {
+    if (Math.round(window.scrollY + window.innerHeight) >= Math.round(document.body.scrollHeight)) {
+      scrollToEnd()
+    }
+  }
 
   const calculatedAge = (birthday) => {
     let today = new Date();
@@ -72,63 +95,73 @@ export default function HomeScreen() {
     return age;
   };
 
-  return (
-    <div className="site-layout-content d-flex flex-wrap justify-content-center">
-      <Modal title="User Detail" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-        <ModalScreen
-          email={modaldata[0]}
-          name={modaldata[1]}
-          last_name={modaldata[2]}
-          phone={modaldata[3]}
-          cell={modaldata[4]}
-          street_number={modaldata[5]}
-          street_name={modaldata[6]}
-          city={modaldata[7]}
-          state={modaldata[8]}
-          country={modaldata[9]}
-          postcode={modaldata[10]}
-          nat={modaldata[11]}
-        />
-      </Modal>
+  // const handleButtonFilter = (e) => {
+  //   console.log(e.target.value);
+  // }
 
-      {loading? <LoadingBox></LoadingBox>
-        :
-        error?<MessageBox>{error}</MessageBox>
-        :<>
-        {posts && posts.results.map((post, index) => (
-            <div key={index}>
-              <div className='d-flex align-self-stretch'>
-                <Card
-                  style={{ width: 280, margin: '8px 8px' }}
-                  actions={[
-                    <EyeOutlined key="setting"
-                      onClick={()=>
-                        showModal(
-                          post.email,
-                          post.name.first,
-                          post.name.last,
-                          post.phone,
-                          post.cell,
-                          post.location.street.number,
-                          post.location.street.name,
-                          post.location.city,
-                          post.location.state,
-                          post.location.country,
-                          post.location.postcode,
-                          post.nat
-                        )}/>,
-                  ]}
-                >
-                  <Meta
-                    avatar={<Avatar src={post.picture.thumbnail} />}
-                    title={[post.name.title + ". " + post.name.first + " " + post.name.last]}
-                    description={[post.email + "\n City : " + post.location.city + "\n Date Of Birthday : " + new Date(post.dob.date).toLocaleString() + " Age : " + calculatedAge(post.dob.date) + " Years Old"]}
-                  />
-                </Card>
+  return (
+    <div>
+      <div className="d-flex justify-content-end align-items-center">
+        <p className="mb-0 mr-3">Filter Nation By : </p>
+        <Button type="primary" value="gb" onClick={props.handleButtonFilter}>GB</Button>
+      </div>
+      <div className="site-layout-content d-flex flex-wrap justify-content-center">
+        <Modal title="User Detail" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel} handleButtonFilter={this.handleButtonFilter}>
+          <ModalScreen
+            email={modaldata[0]}
+            name={modaldata[1]}
+            last_name={modaldata[2]}
+            phone={modaldata[3]}
+            cell={modaldata[4]}
+            street_number={modaldata[5]}
+            street_name={modaldata[6]}
+            city={modaldata[7]}
+            state={modaldata[8]}
+            country={modaldata[9]}
+            postcode={modaldata[10]}
+            nat={modaldata[11]}
+          />
+        </Modal>
+
+        {loading? <LoadingBox></LoadingBox>
+          :
+          error?<MessageBox>{error}</MessageBox>
+          :<>
+          {posts && posts.results.map((post, index) => (
+              <div key={index}>
+                <div className='d-flex align-self-stretch'>
+                  <Card
+                    style={{ width: 280, margin: '8px 8px' }}
+                    actions={[
+                      <EyeOutlined key="setting"
+                        onClick={()=>
+                          showModal(
+                            post.email,
+                            post.name.first,
+                            post.name.last,
+                            post.phone,
+                            post.cell,
+                            post.location.street.number,
+                            post.location.street.name,
+                            post.location.city,
+                            post.location.state,
+                            post.location.country,
+                            post.location.postcode,
+                            post.nat
+                          )}/>,
+                    ]}
+                  >
+                    <Meta
+                      avatar={<Avatar src={post.picture.thumbnail} />}
+                      title={[post.name.title + ". " + post.name.first + " " + post.name.last]}
+                      description={[post.email + "\n City : " + post.location.city + "\n Date Of Birthday : " + new Date(post.dob.date).toLocaleString() + " Age : " + calculatedAge(post.dob.date) + " Years Old"]}
+                    />
+                  </Card>
+                </div>
               </div>
-            </div>
-        ))}
-      </>}
+          ))}
+        </>}
+      </div>
     </div>
   )
 }
